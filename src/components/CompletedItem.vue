242717eaeda3ae8bf62250d9bdf9c1df2da8f2fb
17471 <1747153981@qq.com>
2022-11-15 12:46:33 +0800

    3

--- src/components/CompletedItem.vue
+++ src/components/CompletedItem.vue
@@ -1,13 +1,22 @@
 <template>
-
+  <div class="h-16 mt-2">
+    <div class="flex h-14 container mx-auto w-full justify-between bg-gray-700 rounded-lg">
+      <div class="rounded-l-3xl text-white text-xl">
+        <div class="mt-3 ml-3 text-gray-500">{{ taskContent }}</div>
+      </div>
+      <div class="flex mt-3 mr-3">
+        <svg t="1668414299574" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4374" width="2rem" height="2rem"><path d="M511.950005 512.049995m-447.956254 0a447.956254 447.956254 0 1 0 895.912508 0 447.956254 447.956254 0 1 0-895.912508 0Z" fill="#20B759" p-id="4375"></path><path d="M458.95518 649.636559L289.271751 479.95313c-11.698858-11.698858-30.697002-11.698858-42.39586 0s-11.698858 30.697002 0 42.395859l169.683429 169.68343c11.698858 11.698858 30.697002 11.698858 42.39586 0 11.798848-11.598867 11.798848-30.597012 0-42.39586z" fill="#FFFFFF" p-id="4376"></path><path d="M777.62406 332.267552c-11.698858-11.698858-30.697002-11.698858-42.39586 0L424.158578 643.437164c-11.698858 11.698858-11.698858 30.697002 0 42.39586s30.697002 11.698858 42.39586 0l311.069622-311.069622c11.798848-11.798848 11.798848-30.796992 0-42.49585z" fill="#FFFFFF" p-id="4377"></path></svg>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const activeNames = ref(['1'])
-const handleChange = (val: string[]) => {
-  console.log(val)
+<script>
+export default {
+  name: "item",
+  props: {
+    taskContent: String
+  }
 }
 </script>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -18,7 +18,14 @@
         </div>
       </div>
     </div>
-    <div class="demo-progress; w-2/5 container mx-auto mt-20">
+    <div class="w-2/5 container mx-auto">
+      <el-date-picker
+          v-model="currentDate"
+          type="date"
+          placeholder="Pick a day"
+      />
+    </div>
+    <div class="demo-progress; w-2/5 container mx-auto mt-10">
       <el-progress
           :text-inside="true"
           :stroke-width="30"
@@ -29,14 +36,57 @@
     <div class="h-16 mt-5 ">
       <div class="flex container mx-auto w-2/5 bg-gray-900 rounded-lg">
         <input class="h-14 flex-1 bg-gray-900 rounded-l-3xl outline-white text-white" placeholder="    添加任务">
-        <svg t="1668338684769" class="icon; mt-3 mr-3" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2676" width="2rem" height="2rem"><path d="M925.696 384q19.456 0 37.376 7.68t30.72 20.48 20.48 30.72 7.68 37.376q0 20.48-7.68 37.888t-20.48 30.208-30.72 20.48-37.376 7.68l-287.744 0 0 287.744q0 20.48-7.68 37.888t-20.48 30.208-30.72 20.48-37.376 7.68q-20.48 0-37.888-7.68t-30.208-20.48-20.48-30.208-7.68-37.888l0-287.744-287.744 0q-20.48 0-37.888-7.68t-30.208-20.48-20.48-30.208-7.68-37.888q0-19.456 7.68-37.376t20.48-30.72 30.208-20.48 37.888-7.68l287.744 0 0-287.744q0-19.456 7.68-37.376t20.48-30.72 30.208-20.48 37.888-7.68q39.936 0 68.096 28.16t28.16 68.096l0 287.744 287.744 0z" p-id="2677" fill="#ffffff"></path></svg>
+        <svg @click="dialogVisible = true" t="1668338684769" class="icon; mt-3 mr-3" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2676" width="2rem" height="2rem"><path d="M925.696 384q19.456 0 37.376 7.68t30.72 20.48 20.48 30.72 7.68 37.376q0 20.48-7.68 37.888t-20.48 30.208-30.72 20.48-37.376 7.68l-287.744 0 0 287.744q0 20.48-7.68 37.888t-20.48 30.208-30.72 20.48-37.376 7.68q-20.48 0-37.888-7.68t-30.208-20.48-20.48-30.208-7.68-37.888l0-287.744-287.744 0q-20.48 0-37.888-7.68t-30.208-20.48-20.48-30.208-7.68-37.888q0-19.456 7.68-37.376t20.48-30.72 30.208-20.48 37.888-7.68l287.744 0 0-287.744q0-19.456 7.68-37.376t20.48-30.72 30.208-20.48 37.888-7.68q39.936 0 68.096 28.16t28.16 68.096l0 287.744 287.744 0z" p-id="2677" fill="#ffffff"></path></svg>
       </div>
     </div>
+    <el-dialog
+        v-model="dialogVisible"
+        title="新建任务"
+        width="40%"
+        :before-close="handleClose"
+    >
+      <el-form :model="form" label-width="120px">
+        <el-form-item label="Activity name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="Activity time">
+          <el-col :span="11">
+            <el-date-picker
+                v-model="form.startTime"
+                value-format="YYYY-MM-DD"
+                type="date"
+                placeholder="Pick a date"
+                style="width: 100%"
+            />
+          </el-col>
+          <el-col :span="2" class="text-center">
+            <span class="text-gray-500">-</span>
+          </el-col>
+          <el-col :span="11">
+            <el-date-picker
+                v-model="form.endTime"
+                value-format="YYYY-MM-DD"
+                type="date"
+                placeholder="Pick a date"
+                style="width: 100%"
+            />
+          </el-col>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="onSubmit">
+          Confirm
+        </el-button>
+      </span>
+      </template>
+    </el-dialog>
     <item v-for="task in doingTask" :key="doingTask[task]" :task-content="task"></item>
     <div class="demo-collapse">
       <el-collapse>
         <el-collapse-item title="已完成" name="1">
-          <item v-for="task in doingTask" :key="doingTask[task]" :task-content="task"></item>
+          <c-item v-for="task in doingTask" :key="doingTask[task]" :task-content="task"></c-item>
         </el-collapse-item>
       </el-collapse>
     </div>
@@ -48,6 +98,8 @@
 <script>
 import item from "../components/unCompletedItem.vue";
 import cItem from "../components/CompletedItem.vue"
+import {ElMessageBox} from "element-plus";
+import service from "../utils/axios.js";
 export default {
   components: {
     item,
@@ -56,13 +108,128 @@ export default {
   name: "index",
   data() {
     return {
-      doingTask: ["为爱冲锋","为爱冲锋","为爱冲锋"]
+      doingTask: ["为爱冲锋","为爱冲锋","为爱冲锋"],
+      value2: '',
+      currentDate: null,
+      currentYear: '',
+      currentMonth: '',
+      currentDay: '',
+      currentWeek: '',
+      dialogVisible: false,
+      form: {
+        taskId: '',
+        name: '',
+        status: '',
+        startTime: '',
+        endTime: '',
+      }
+    }
+  },
+  watch: {
+    currentDate() {
+      console.log('------------------------------------------------------');
+      console.log('当前日期' + this.currentDate);
+      let date = new Date(this.currentDate);
+      console.log('年份'+date.getFullYear());
+      console.log('月份'+date.getMonth());
+      console.log('星期'+date.getDay());
+      console.log('日期'+date.getDate());
+      this.currentMonth = date.getMonth();
+      this.currentDay = date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate();
+      switch (date.getDay()){
+        case 0:
+          this.currentWeek = "Sunday";
+          break;
+        case 1:
+          this.currentWeek = "Monday";
+          break;
+        case 2:
+          this.currentWeek = "TuesDay";
+          break;
+        case 3:
+          this.currentWeek = "Wednesday";
+          break;
+        case 4:
+          this.currentWeek = "Thursday";
+          break;
+        case 5:
+          this.currentWeek = "Friday";
+          break;
+        case 6:
+          this.currentWeek = "Saturday";
+          break;
+      }
+      switch (date.getMonth()) {
+        case 0:
+          this.currentMonth = "Jan";
+          break;
+        case 1:
+          this.currentMonth = "Feb";
+          break;
+        case 2:
+          this.currentMonth = "Mar";
+          break;
+        case 3:
+          this.currentMonth = "Apr";
+          break;
+        case 4:
+          this.currentMonth = "May";
+          break;
+        case 5:
+          this.currentMonth = "Jun";
+          break;
+        case 6:
+          this.currentMonth = "Jul";
+          break;
+        case 7:
+          this.currentMonth = "Aug";
+          break;
+        case 8:
+          this.currentMonth = "Sep";
+          break;
+        case 9:
+          this.currentMonth = "Oct";
+          break;
+        case 10:
+          this.currentMonth = "Nov";
+          break;
+        case 11:
+          this.currentMonth = "Dec";
+          break;
+      }
+      console.log("------------------------------------------------")
     }
   },
   methods: {
-    format : (percentage) => (percentage === 100 ? 'Full' : `${percentage}%`)
+      format : (percentage) => (percentage === 100 ? 'Full' : `${percentage}%`),
+    getDate() {
+      console.log(this.value2)
+    },
+    handleClose( ) {
+      ElMessageBox.confirm('您确定取消添加任务吗？')
+          .then(() => {
+            this.dialogVisible = false
+          })
+          .catch(() => {
+
+          })
+    },
+    onSubmit() {
+      this.dialogVisible = false;
+      console.log(this.form.name)
+      console.log(this.form.startTime);
+      console.log(this.form.endTime);
+      service.post('/task/insertTask', this.form)
+          .then( res => {
+            console.log(res.data)
+          })
+     }
   },
   created() {
+    service.get('/test/hello')
+        .then(res => {
+          console.log(res.data)
+        })
     console.log(123);
   }
 }
@@ -76,9 +243,12 @@ export default {
 }
 .demo-collapse{
   width: 40%;
+  margin: 0 auto;
 }
 :deep(.el-collapse-item__header) {
-  color: silver;
+  color: white;
+  font-size:larger;
+  font-weight: bolder;
   background-color: rgb(31,41,55);
 }
 :deep(.el-collapse-item__wrap) {
